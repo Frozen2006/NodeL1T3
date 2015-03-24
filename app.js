b6b1212edb1192786e5c246468e8efe7e3b3658d
Ilya Starostin <ilya.starostin@itechart-group.com>
2015-03-24 19:52:09 +0300

    Uploading

---
+++ app.js
@@ -0,0 +1,12 @@
+﻿/*
+ * Use express with static middleware easiest way :)
+ */
+var port = process.env.PORT || 1337;
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public/'));
+
+app.listen(port, function () {
+    console.log('Server started at port ' + port);
+})
